0272057ced599f6b9d805dee5b9c00d656a62b2f
honzavalustik <valustik@gmail.com>
2022-01-21 21:03:03 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+// Write Javascript code!,,l,l,
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
